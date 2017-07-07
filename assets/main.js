0dc2bd04be498af6ef2c1ca9cd18f9d27c0c725a
macarena baltra <macarena.bc2011@gmail.com>
2017-07-06 21:26:17 -0400

    Intentos JQuery

--- assets/main.js
+++ assets/main.js
@@ -2,5 +2,32 @@
 
 $(document).ready(function() {
     $('.carousel').carousel({interval: 5000}); // cambia con tiempo definido - Va cambiando solito 5 segundos
-    $('#myCarousel .slide').animate({'margin-left': -720}) // para que se mueva hacia la izquierda
+    
   });
+
+
+/* INTENTOS CUECK
+
+$(document).ready(function() {
+    var transicion = 5000;
+    var valocidad = 1000;
+    var pausa = 7000;
+    var diapoImg = 1;
+
+    var $slider = $('#myCarousel');
+    var $sliderCaja = $slider.find('.slides');
+    var $slider2 = $sliderCaja.find('.slide');
+
+    setInterval(function(){
+    	$sliderCaja.animate({'margin-left': '-=' + transicion}, valocidad, function(){
+    		diapoImg++;
+    		if(diapoImg === $slider2.length){
+    			diapoImg = 1;
+    			sliderCaja.css('margin-left', 0);
+    		}
+    	});
+    }, pausa);
+    
+  });
+
+*/
