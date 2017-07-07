448759e214cd56b89e656d0b6528aa1c46f9702c
macarena baltra <macarena.bc2011@gmail.com>
2017-07-06 20:57:29 -0400

    Agregando JQuery

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,6 @@
- $(document).ready(function() {
-    $('.carousel').carousel({interval: 5000}); // cambia con tiempo definido - Va cambiando solito
+/* JQuery */ 
+
+$(document).ready(function() {
+    $('.carousel').carousel({interval: 5000}); // cambia con tiempo definido - Va cambiando solito 5 segundos
+    $('#myCarousel .slide').animate({'margin-left': -720}) // para que se mueva hacia la izquierda
   });
